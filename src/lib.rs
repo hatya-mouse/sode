89a0d3f9a407cc5a5a70fddd1ce3c1c9670db905
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-21 22:45:34 +0900

    docs: add documentation for decode, encode, impls modules; add impls.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+/// The definition of `Decode` trait, `FieldDecoder` and `ValueDecoder` structs.
 mod decode;
+/// The definition of `Encode` trait and `Encoder` struct.
 mod encode;
+/// The implementetion of `Decode` and `Encode` traits for primitive types.
+mod impls;
 pub(crate) mod primitives;
 
-pub use decode::Decode;
-pub use encode::Encode;
+pub use decode::{Decode, DecodeError, FieldDecoder, ValueDecoder};
+pub use encode::{Encode, EncodeError, Encoder};
