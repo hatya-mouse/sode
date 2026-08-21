ab62b14ead16ac6b49acb1b4bf5da2557a15e19a
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-21 22:52:35 +0900

    feat: implement Decode for u8; add more doc comments

--- src/impls.rs
+++ src/impls.rs
@@ -1,1 +1,9 @@
+use crate::{Decode, DecodeError, ValueDecoder};
 
+impl Decode for u8 {
+    fn decode(d: &mut ValueDecoder) -> Result<Self, DecodeError> {
+        let mut buf = [0u8; 1];
+        d.read_exact(&mut buf)?;
+        Ok(buf[0])
+    }
+}
