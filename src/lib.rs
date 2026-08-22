faab9c684e354c9cb5f59cec4644e7cfb3faa144
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-22 19:25:07 +0900

    docs: feat add example to root doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! # sode
+//!
+//! `sode` is a simple and small binary decoding / encoding crate.
+//!
+//! ## Example
+//!
+//! ### Decoding
+//!
+//! ```rust
+//! use sode::{Decode, ValueDecoder};
+//!
+//! struct User {
+//!     name: String,
+//!     age: u32,
+//!     id: u64,
+//! }
+//!
+//! impl Decode for User {
+//!     fn decode(d: &mut ValueDecoder) -> Result<Self, DecodeError> {
+//!         let name = String::decode(d)?;
+//!         let age = u32::decode(d)?;
+//!         let id = u64::decode(d)?;
+//!     }
+//! }
+//! ```
+
 /// The definition of `Decode` trait, `FieldDecoder` and `ValueDecoder` structs.
 mod decode;
 /// The definition of `Encode` trait and `Encoder` struct.
